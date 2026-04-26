ff97f362963b0eb71e252521e95df24bfa9f97fc
MakirovicKH <mahirrx-ab205@code.edu.az>
2026-04-26 22:40:15 +0400

    Add exception middleware

---
+++ PayFlowEngine/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace PayFlowEngine.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ArgumentException ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Response.ContentType = "application/json";
+
+            var response = new
+            {
+                success = false,
+                error = ex.Message
+            };
+
+            var json = JsonSerializer.Serialize(response);
+            await context.Response.WriteAsync(json);
+        }
+        catch (Exception)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var response = new
+            {
+                success = false,
+                error = "Internal server error"
+            };
+
+            var json = JsonSerializer.Serialize(response);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
